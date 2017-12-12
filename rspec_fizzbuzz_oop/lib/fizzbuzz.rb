4ef713a4214e58bd0dbd690bca4c764ea261c657
solufemi91 <stevenoo@spartaglobal.com>
2017-12-12 12:21:03 +0000

    fizbuzz solution

--- rspec_fizzbuzz_oop/lib/fizzbuzz.rb
+++ rspec_fizzbuzz_oop/lib/fizzbuzz.rb
@@ -1,4 +1,28 @@
 class Fizzbuzz
+  attr_accessor :fizzbuzz_array
 
+  def initialize
+    @fizzbuzz_array = []
+  end
+
+  def divideby?(num,n)
+    (num % n).zero?
+  end
+
+  def fizzbuzz_iterator(range_from, range_to)
+    (range_from..range_to).each do |i|
+      if divideby?(i,15)
+        @fizzbuzz_array << 'fizzBuzz'
+      elsif divideby?(i,5)
+        @fizzbuzz_array << 'buzz'
+      elsif divideby?(i,3)
+        @fizzbuzz_array << 'fizz'
+      else
+        @fizzbuzz_array << i
+      end
+
+
+    end
+  end
 
 end
--- rspec_fizzbuzz_oop/spec/fizzbuzz_spec.rb
+++ rspec_fizzbuzz_oop/spec/fizzbuzz_spec.rb
@@ -2,6 +2,34 @@ require 'fizzbuzz'
 
 describe Fizzbuzz do
 
+  before(:each) do
+    @fb = Fizzbuzz.new
+  end
+
+  it 'should respond true if the numbers are divisible by three' do
+    expect(@fb.divideby?(6,3)).to be true
+  end
+
+  it 'should respond false if the numbers are not divisible three' do
+    expect(@fb.divideby?(7,3)).to be false
+  end
+
+  it 'should respond true if the numbers are divisible by five' do
+    expect(@fb.divideby?(5,5)).to be true
+  end
+
+  it 'should respond false if the numbers are not divisible by five' do
+    expect(@fb.divideby?(6,5)).to be false
+  end
+
+  it 'should correctly apply fizbuzz to a given range' do
+    @fb.fizzbuzz_iterator(1,15)
+
+    expect(@fb.fizzbuzz_array[2]).to eq 'fizz'
+    expect(@fb.fizzbuzz_array[4]).to eq 'buzz'
+    expect(@fb.fizzbuzz_array[-1]).to eq 'fizzBuzz'
+  end
+
+
 
 end
-

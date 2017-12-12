4af387274f128d4cc0a835a732be3d531eeeed4a
solufemi91 <stevenoo@spartaglobal.com>
2017-12-12 21:54:25 +0000

    completed euler question 2

--- euler_q2/spec/euler_q2_spec.rb
+++ euler_q2/spec/euler_q2_spec.rb
@@ -6,8 +6,12 @@ describe Euler_q2 do
     @q2 = Euler_q2.new
   end
 
-  it 'find the sum of all the even numbers in the Fibonacci sequence within a specified range' do
+  it 'find the sum of all the even numbers in the Fibonacci sequence that do not exceed a specified value, in this case 35' do
+    expect(@q2.sumOfEvenFibonacci(35)).to eq 44
+  end
 
+  it 'find the sum of all the even numbers in the Fibonacci sequence that do not exceed a specified value, in this case 5' do
+    expect(@q2.sumOfEvenFibonacci(5)).to eq 2
   end
 
 
